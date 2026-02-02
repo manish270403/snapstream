7f02e91ba77385f7fdd874241eebfc9defcdac2f
harsh <manish57558@gmail.com>
2026-02-02 21:55:48 +0530

    routes and controller 1

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+//routes import
+import userRouter from './routes/user.route.js';
 
 const app = express();
 
@@ -20,5 +22,13 @@ app.use(
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+
+
+//routes declaration
+app.use("/api/v1/users",userRouter);
+
+//http://localhost:8000/api/v1/users/register
 
 export { app };
